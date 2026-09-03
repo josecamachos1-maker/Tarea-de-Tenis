a550d92b599d06b7f7a1851fcf352c1582ac86df
pochis <jose.camacho.s1@ucb.edu.bo>
2026-09-03 01:19:25 -0400

    Feat: ultima implementacion de presenter y html basico para que funcione la tarea larga

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,54 @@
-import sumar from "./sumador";
+import { Tennis } from "./Tennis.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const tennis = new Tennis();
+const botonJ1 = document.querySelector("#jugador1-button");
+const botonJ2 = document.querySelector("#jugador2-button");
+const resultado = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+let j1 = 0;
+let j2 = 0;
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+botonJ1.addEventListener("click", () => {
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+    if(j1 == 0)
+    {
+        j1 = 15;
+    }
+    else if(j1 == 15)
+    {
+        j1 = 30;
+    }
+    else if(j1 == 30)
+    {
+        j1 = 40;
+    }
+    else
+    {
+        j1 = j1 + 10;
+    }
+
+    resultado.innerHTML = tennis.score(j1, j2);
+});
+
+
+botonJ2.addEventListener("click", () => {
+
+    if(j2 == 0)
+    {
+        j2 = 15;
+    }
+    else if(j2 == 15)
+    {
+        j2 = 30;
+    }
+    else if(j2 == 30)
+    {
+        j2 = 40;
+    }
+    else
+    {
+        j2 = j2 + 10;
+    }
+
+    resultado.innerHTML = tennis.score(j1, j2);
 });
